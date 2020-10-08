8fc199bbe50fc31a3111972a2ee8ad2ebdd85ff8
Shuyi Fang <66406517+Terence0101@users.noreply.github.com>
2020-10-08 13:34:49 +0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,21 @@
+import re
+import requests
+from pyquery import PyQuery as pq
+
+date = input()
+
+response = requests.get('https://www.officialcharts.com/charts/singles-chart/{}/7501/'.format(date.replace('/','')))
+doc = pq(response.text)
+
+info = []
+singer = []
+Pos = []
+for each_tr in doc('.artist').items():
+    singer.append(each_tr.text())
+for each_tr in doc('.title').items():
+    info.append(each_tr.text())
+for each_tr in doc('.position').items():
+    Pos.append(each_tr.text())
+
+for i in range (5):
+    print('{}.{}-{}'.format(Pos[i],info[i],singer[i]))
